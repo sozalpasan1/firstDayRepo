ff42ba6ad2f0faee0ec006d0a43a38738fb68cd2
sean <sozalpasan1@hwemail.com>
2024-09-05 19:08:19 -0700

    Added .gitignore + some other files
    Add hidden and non-hidden files using chosen writing method; create .gitignore file

--- MyFileWriter.java
+++ MyFileWriter.java
@@ -1,53 +1,27 @@
-//what the sigma
 import java.io.*;
 import java.nio.file.*;
 import java.nio.charset.StandardCharsets;
 
 public class MyFileWriter{
     public static void main(String[] args) {
-        String data = "Hello, World!";
-        String fileName1 = "example1.txt";
-        String fileName2 = "example2.txt";
-        String fileName3 = "example3.txt";
-        String fileName4 = "example4.txt";
-        String fileName5 = "example5.txt";
-
-        // 1. Using FileWriter
-        try (FileWriter writer = new FileWriter(fileName1)) {
-            writer.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 2. Using BufferedWriter
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName2))) {
-            bufferedWriter.write(data);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 3. Using FileOutputStream
-        try (FileOutputStream outputStream = new FileOutputStream(fileName3)) {
-            outputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 4. Using BufferedOutputStream
-        try (BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName4))) {
-            bufferedOutputStream.write(data.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // 5. Using Files (java.nio.file)
+        String passwordShush = "if you find this ur sigma";
+        String confidata = "confidentialdata";
         try {
-            Files.write(Paths.get(fileName5), data.getBytes(StandardCharsets.UTF_8));
+            Path hiddenFile = Paths.get(".dontfindme.txt");
+            Files.createFile(hiddenFile);
+            Files.write(hiddenFile, passwordShush.getBytes(StandardCharsets.UTF_8));
+            
+
+            Path hiddenDir = Paths.get(".hideme");
+            Files.createDirectory(hiddenDir);
+
+            Path confidatafile = hiddenDir.resolve("confidata.dat");
+            Files.createFile(confidatafile);
+            Files.write(confidatafile, confidata.getBytes(StandardCharsets.UTF_8));
+            
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
-/*
- * hi this is just testing
- */
